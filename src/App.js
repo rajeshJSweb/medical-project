4f10bf1295d8d4d05a3791bf3413f5fd371fdcba
rajeshJS <rjshkhoksi26@gmail.com>
2021-10-17 23:22:14 +0600

    Home commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter,Switch,Route } from 'react-router-dom';
+import Header from './Firebase/components/Header/Header';
+import Home from './Firebase/components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h>Medibles</h>
+      <BrowserRouter>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route exact path="/home">
+            <Home></Home>
+          </Route>
+          <Route>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDpr7hOEOW8HquSOsehqTbQnBgyAJziyuM",
+    authDomain: "medibles-web.firebaseapp.com",
+    projectId: "medibles-web",
+    storageBucket: "medibles-web.appspot.com",
+    messagingSenderId: "856439000395",
+    appId: "1:856439000395:web:954994036bb5bdfbceacce"
+  };
+
+export default firebaseConfig;
