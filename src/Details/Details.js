01c33b0f263630fe150676cf90855c0d0f1ddd97
rajeshJS <rjshkhoksi26@gmail.com>
2021-10-20 23:51:44 +0600

    restore file

---
+++ src/Details/Details.js
@@ -0,0 +1,26 @@
+/* import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+
+const Details = () => {
+    const { id } = useParams();
+
+    const [serviceDetails, setServicesDetails] = useState([]);
+
+    useEffect(() => {
+        fetch('/../services.json')
+            .then(res => res.json())
+        .then(data => setServicesDetails(data))
+    },[])
+
+    useEffect(() => {
+        const foundData = serviceDetails.find((service) => service.login.id === id)
+        console.log(foundData);
+    },[])
+    return (
+        <div>
+            <h2>This is details{id}</h2>
+        </div>
+    );
+};
+
+export default Details; */
